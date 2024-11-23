5f5e8ba9b19fdf3f77e6360e4b6374f51b609986
Andre Proenca <andreproenca@hotmail.com>
2024-11-23 13:40:21 -0400

    first commit

---
+++ src/main/java/com/softcraft/sfevento/SfeventoApplication.java
@@ -0,0 +1,13 @@
+package com.softcraft.sfevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfeventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcraft/sfevento/SfeventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcraft.sfevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfeventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
