36dbd277ba491642840174e1fca4f53280684f1b
Andre Proenca <andreproenca@hotmail.com>
2024-11-24 10:24:31 -0400

    associacao tb_bloco, tb_atividade

--- src/main/java/com/softcraft/sfevento/entities/Atividade.java
+++ src/main/java/com/softcraft/sfevento/entities/Atividade.java
@@ -3,7 +3,9 @@ package com.softcraft.sfevento.entities;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_atividade")
@@ -12,13 +14,19 @@ public class Atividade {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    @Column(columnDefinition = "TEXT")
     private String descricao;
     private Double preco;
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
-    @OneToMany(mappedBy = "bloco")
+    @OneToMany(mappedBy = "atividade")
     private List<Bloco> blocos = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
 
     public Atividade() {}
 
@@ -63,5 +71,9 @@ public class Atividade {
 
     public List<Bloco> getBlocos() {
         return blocos;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
     }
 }
--- src/main/java/com/softcraft/sfevento/entities/Participante.java
+++ src/main/java/com/softcraft/sfevento/entities/Participante.java
@@ -1,12 +1,21 @@
 package com.softcraft.sfevento.entities;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_participante")
 public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     @Column(unique = true)
     private String email;
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
 
     public Participante() {}
 
@@ -38,5 +47,9 @@ public class Participante {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
     }
 }
